087c2cba687b51891717e30bd22d3c6f02055f7f
pankace <44595774+pankace@users.noreply.github.com>
2025-04-03 13:40:59 +0700

    Refactor: repo cloud run

--- terraform/main.tf
+++ terraform/main.tf
@@ -279,11 +279,12 @@ resource "google_pubsub_subscription" "subscription" {
   # Add lifecycle block for subscription
   lifecycle {
     ignore_changes = [
-      name,  # For storage bucket, topic, etc.
-      dataset_id,  # For BigQuery dataset
-      account_id   # For service account
+      name,
+      topic
     ]
+  }
 }
+
 # Allow public access to the extract function
 resource "google_cloud_run_service_iam_binding" "extract_public" {
   location = var.region
